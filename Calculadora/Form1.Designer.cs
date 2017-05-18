94d284a688d4d66a6328a094718e81579c47c8be
rodrigoaives <rodrigoaives@outlook.com>
2017-05-18 19:43:04 -0300

    Teste.

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -54,7 +54,7 @@ namespace Calculadora
             // txtValor
             // 
             this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtValor.Location = new System.Drawing.Point(27, 30);
+            this.txtValor.Location = new System.Drawing.Point(27, 26);
             this.txtValor.Name = "txtValor";
             this.txtValor.Size = new System.Drawing.Size(288, 35);
             this.txtValor.TabIndex = 0;
